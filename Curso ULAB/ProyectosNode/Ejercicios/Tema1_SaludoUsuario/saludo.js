7f438f051e932e30dc9f041cfc3553f92234337a
Mario <yosoymariomoya@gmail.com>
2019-04-11 13:41:05 +0200

    ejercicios node

--- Curso ULAB/ProyectosNode/Ejercicios/Tema1_SaludoUsuario/saludo.js
+++ Curso ULAB/ProyectosNode/Ejercicios/Tema1_SaludoUsuario/saludo.js
@@ -2,6 +2,6 @@
 const os = require('os');
 
 console.log("Hola "+os.userInfo().username);
-
+// const os = require('os');
 // const usuario = os.userInfo();
 // console.log("Hola "+usuario.username);
